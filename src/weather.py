1da8d9b3a7d6a5a72155faa9c9b42ae6d3a53ea8
worldchampio <magnusnilsenrandaberg@hotmail.com>
2022-02-15 23:36:49 +0100

    error handling

--- src/weather.py
+++ src/weather.py
@@ -12,11 +12,8 @@ attached to the desired location. Four observation types
 can then be selected.
 
 Data is currently requested for the current day.
-
-TODO:
-Fix 412/404, make new source request if first returns 412/404 ( replicate main())
-
 """
+
 class Weather:
     def __init__(self, end_src, end_observ) -> None:
         try:
@@ -61,23 +58,17 @@ class Weather:
         
             self.plotdata(observations, stationholder, source_id, now)
         except ValueError:
-            """
-            ValueError is thrown despite nothing going wrong with data retrieval,
-            hence nothing needs to be handled
-            """
-            print("")
+            print("Valueerror raised")
+            main()
         except TypeError:
-            """
-            Same for TypeError
-            """
-            print("")
-    
+            print("Typeerror raised")
+            main()
+
     def get_src(self):
-        psrc = {
+        return {
             'name': str(input("Type location: [yme, statfjord a/b/c, troll a/b/c, .. etc] \n"))
         }
-        return psrc 
-    
+        
     def getdata(self, endpoint, parameters,_client_id): # Done
         client_id = _client_id
 
@@ -85,24 +76,12 @@ class Weather:
         # Extract JSON data
         json = r.json()
         # Check if the request worked, print out any errors
-        try:
-            if r.status_code == 200:
-                #print('Data retrieved from ' + endpoint)
-                data = json['data']
-                return np.asarray(data)
-            """"
-            else:
-                print('Error! Returned status code %s' % r.status_code)
-                print('Message: %s' % json['error']['message'])
-                print('Reason: %s' % json['error']['reason'])
-            """
+        if r.status_code == 200:
+            data = json['data']
+            return np.asarray(data)
         # Handle source with no available data
-        except r.status_code==412 or r.status_code==404 and 'name' in parameters:
-            new_src = input("No data, try another name: ")
-            new_param = {'name':new_src}
-            return np.asarray(self.getdata(endpoint,new_param,client_id))
         else:
-            print("No data found.")
+            print("No data found. ("+r.status_code+")")
             main()
 
     def plotdata(self, data, stationholder, source_id, t_now): #Done
@@ -124,7 +103,7 @@ class Weather:
         if df['unit'][1]=='m/s':
             unit_label = '[kts]'
             for j in range(len(df['value'])):
-                df['value'][j]=df['value'][j]*1.94384449 #to kts
+                df['value'][j]=df['value'][j]*1.94384449
 
         # Calculate plot limits
         #   Find the max value of the row, round
@@ -134,14 +113,7 @@ class Weather:
         
         # Scale limits
         ylim_param = (float(mag_min)-0.4*float(mag_min),float(mag_max)+0.1*float(mag_max))
-        
-        """ Handle exception if min==max
-        If data is supposed to be available,
-        but for some reason isnt, df['value']
-        will be all zeros, in which case the
-        scaling method is set to None to make
-        matplotlib handle it (ie eliminate error messages)
-        """
+
         if ylim_param == (0,0):
             ylim_param = None
         
@@ -161,17 +133,17 @@ class Weather:
         fig, ax = plt.subplots()
         ax.plot(x, y)
 
-        ax.set(xlabel='time [hh:mm]', 
-                ylabel=unit_label, 
-                ylim=ylim_param,
-                title= 'Displaying '+mag_label+ ' at '+stationholder+'.\n Data for '+ t_now +' from '+source_id+'. Max: '+mag_max+', Min: '+mag_min )
-        ax.grid()
-        plt.setp(ax.get_xticklabels(), rotation=60, ha='right')
-        #This line sets label spacing, FIX ITTTTTT #TODO TODO
-        plt.setp(ax.get_xticklabels()[::2], visible=False)
-        
-        # This file will be deleted next time the 
-        # bash script is executed
+        # Display every nth label
+        n = int(len(y)/8)
+        for index, label in enumerate(ax.xaxis.get_ticklabels()):
+            if int(index) % n != 0:
+                label.set_visible(False)
+
+        # Plot options
+        plt.title('Displaying '+mag_label+ ' at '+stationholder+'.\n Data for '+ t_now +' from '+source_id+'. Max: '+mag_max+', Min: '+mag_min )
+        plt.xlabel('Time [s]')
+        plt.ylabel(unit_label)
+        plt.grid()
         fig.savefig("Plot.png")
         
 def main():    
