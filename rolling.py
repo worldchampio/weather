a6fd0a63be19e9bf6e529a131bea87cd7c2390ed
Magnus Nilsen <magnusnilsenrandaberg@hotmail.com>
2022-09-11 14:49:27 +0200

    i forgot

--- rolling.py
+++ rolling.py
@@ -1,23 +1,31 @@
+"""
+FIFO queue maintaining length n
+"""
 class Queue:
-    def __init__(self,limit):
-        self.limit = limit
-        self.data = []
-    def insert(self, entry):
-        self.data.append(entry)
-        if len(self.data) > self.limit:
-            self.data.pop(0)
+    def __init__(self, n):
+        self.n = n
+        self.queue = []
+    def insert(self, element):
+        self.queue.append(element)
+        if len(self.queue) > self.n:
+            self.queue.pop(0)
     def average(self):
         sum = 0.0
-        for element in self.data:
-            sum+=element
-        return sum/len(self.data)
-
+        for element in self.queue: sum+=element
+        return sum/len(self.queue)
+"""
+Calculate the rolling average for every
+element in a dataset with a windowsize
+"""
 class RollingAverage:
-    def __init__(self, data, limit):
+    def __init__(self, dataset, windowSize):
+        if windowSize > len(dataset):
+            print("windowsize should be smaller than dataset length.")
+            return []
         self.rollingAverage = []
-        queue = Queue(limit)
-        for element in data:
+        queue = Queue(windowSize)
+        for element in dataset:
             queue.insert(element)
             self.rollingAverage.append(queue.average())
-    def getData(self):
+    def getData(self): 
         return self.rollingAverage
--- weather.py
+++ weather.py
@@ -11,65 +11,69 @@ a source id for a manually inputted location is
 requested and used to request data using the source id
 attached to the desired location. Four observation types
 can then be selected.
-
-Data is requested for the current day.
 """
-# Auxiliary Input functions
-def getElementInput():
-    elementNames=['sea_water_speed','sea_surface_wave_significant_height','wind_speed','air_temperature']
-    i = int(input('Plot options:\nCurrent[1], Wave Hs[2], Wind[3], Temp[4]: '))
-    return str(elementNames[i-1])
-def getSourceInput():
-    supportedSources="""
-        Yme
-        Statfjord a/b/c
-        Troll a/b/c
-        Draugen
-        Sola\n"""
-    print("List of some updated sources:\n%s"%supportedSources)
-    sourceName = str(input("Type location: "))
-    if sourceName in {"exit","stop","out","break","abort","^C"}: exit()
-    return sourceName
-
 class Weather:
     def __init__(self, 
-        source  ='',
-        element ='',
+        source  = '',
+        element = '',
         date    = '',
-        windowSizes = [5,10,15,20] 
+        windowSizes = [5],
     ):
+        self.fig, self.ax = plt.subplots()
         self.windowSizes = windowSizes
         with open("secret.txt", "r") as file: self.clientID = file.readline()
         try:
-            if not source: source = getSourceInput()
-            if not element: element = getElementInput()
+            if not source: source = self.getSourceInput()
+            if not element: element = self.getElementInput()
             sourceEndpoint      = 'https://frost.met.no/sources/v0.jsonld?'
             observationEndpoint = 'https://frost.met.no/observations/v0.jsonld'
             pd.set_option('mode.chained_assignment',None)
             sensorsystem = self.requestData(sourceEndpoint,{'name':source})
-            sourceID     = sensorsystem[0]['id']
-            sourceOwner  = sensorsystem[0]['name']
-            sourceOwner  = str(sourceOwner[0]+sourceOwner[1:len(sourceOwner)].lower())
-            print("Found source %s (%s)" % (sourceID,sourceOwner))
+            sourceOwner   = sensorsystem[0]['name']
+            self.sourceOwner  = str(sourceOwner[0]+sourceOwner[1:len(sourceOwner)].lower())
+            self.sourceID = sensorsystem[0]['id']
+            #print("Found source %s (%s)" % (self.sourceID,self.sourceOwner))
             observationParameters = {
-                'sources'      : sourceID,
+                'sources'      : self.sourceID,
                 'elements'     : element,
                 'referencetime': self.formatDate(date)
             }
             observations = self.requestData(observationEndpoint,observationParameters)
-            self.plotData(observations, sourceOwner, sourceID)
+            self.formatData(observations)
+            self.reportInfo()
+            self.plotData()
         except Exception as e:
             print("Error: \n\t%s" %(e))
             main()
         except KeyboardInterrupt:
             exit()
 
+    def getElementInput(self):
+        elementNames=['sea_water_speed','sea_surface_wave_significant_height','wind_speed','air_temperature']
+        i = int(input('Plot options:\nCurrent[1], Wave Hs[2], Wind[3], Temp[4]: '))
+        return str(elementNames[i-1])
+   
+    def getSourceInput(self):
+        supportedSources="""
+            Yme
+            Statfjord a/b/c
+            Troll a/b/c
+            Draugen
+            Sola\n"""
+        print("List of some updated sources:\n%s"%supportedSources)
+        sourceName = str(input("Type location: "))
+        if sourceName in {"exit","stop","out","break","abort","^C"}: exit()
+        return sourceName
+
+    def reportInfo(self):
+        print("Found source %s (%s)" % (self.sourceID,self.sourceOwner))
+        print("Latest entry: \t%s (%i entries)" %(str(self.time.iloc[-1]),self.time.size))
+
     def formatDate(self, today):
         if not today: today = date.today()
         else: today = parser.parse(today)
         now   = today.strftime("%Y-%m-%d")
-        day   = int(today.strftime("%d"))+1
-        tomorrow = today.strftime("%Y-%m-")+str(day)
+        tomorrow = today.strftime("%Y-%m-")+str(int(today.strftime("%d"))+1)
         self.now = now
         return now+'/'+tomorrow
 
@@ -78,7 +82,20 @@ class Weather:
         if r.status_code == 200: return np.asarray(r.json()['data'])
         else: print("Error %i, %s" %(r.status_code,r.reason))
     
-    def plotData(self, data, sourceOwner, source_id):
+    """
+    Create a dataset and corresponding legends with calculated
+    rolling average of the raw data if any windowsize is given in c-tor.
+    [module @rolling.py]
+    """
+    def addRollingAverages(self):
+        legends = ["Raw data"]
+        for windowSize in self.windowSizes:
+            legends.append("wSize: %i" %windowSize)
+            averageData = rolling.RollingAverage(self.data,windowSize).getData()   
+            self.ax.plot(self.time, averageData)
+        return legends
+
+    def formatData(self, data):
         df = pd.DataFrame()
         for i in range(len(data)):
             row = pd.DataFrame(data[i]['observations'])
@@ -86,52 +103,51 @@ class Weather:
             row['sourceId']      = data[i]['sourceId']
             df = pd.concat([df,row])
         df = df.reset_index()
-        unit_label = '['+df['unit'][1]+']'
+        self.unitLabel = '['+df['unit'][1]+']'
+        
         # Convert m/s to kts (Is only done for wind and current)
         if df['unit'][1]=='m/s':
-            unit_label = '[kts]'
+            self.unitLabel = '[kts]'
             for j in range(len(df['value'])):
                 df['value'][j]=df['value'][j]*1.94384449
+        
         # Calculate plot limits
-        dataMax = str(round(max(df['value']),1))
-        dataMin = str(round(min(df['value']),1))
-        # Scale limits
-        ylim_param = (float(dataMin)-0.4*float(dataMin),float(dataMax)+0.1*float(dataMax))
-        if ylim_param == (0,0):
-            ylim_param = None
+        self.dataMax = str(round(max(df['value']),1))
+        self.dataMin = str(round(min(df['value']),1))
+        
         # Observation name formatting
-        displayLabel = df['elementId'][1].replace('_',' ')
-        timeData = df.loc[:,'referenceTime']
-        observationData = df['value']
-        # Remove unecessary digits from timestamp to get format -> [hh:mm]
-        for entry in range(0, len(timeData)):
-            timeData[entry] = timeData.loc[entry][:-8]
-            timeData[entry] = timeData.loc[entry][11:]
-        latestTime = timeData.iloc[-1]
-        print("Latest entry: \t%s (%i entries)" %(str(latestTime),timeData.size))
-        fig, ax = plt.subplots()
-        ax.plot(timeData, observationData)
+        self.displayLabel = df['elementId'][1].replace('_',' ')
+        self.time = df.loc[:,'referenceTime']
+        self.data = df['value']
 
-        legends = ["Raw data"]
-        for i in self.windowSizes:
-            legends.append("wSize: %i" %i)
-            avg = rolling.RollingAverage(observationData,i)
-            averageData = avg.getData()
-            ax.plot(timeData, averageData)
+        # Remove unecessary digits from timestamp to get format -> [hh:mm]
+        for entry in range(0, len(self.time)):
+            self.time[entry] = self.time.loc[entry][:-8]
+            self.time[entry] = self.time.loc[entry][11:]
+
+    def spaceLabels(self):
         # Display every nth label
-        n = int(len(observationData)/8)
-        for index, label in enumerate(ax.xaxis.get_ticklabels()):
-            if int(index) % n != 0:
-                label.set_visible(False)
-        plt.title('Displaying '+displayLabel+ ' at '+sourceOwner+'.\n Data for '+ self.now +' from '+source_id+'. Max: '+dataMax+', Min: '+dataMin )
+        n = int(len(self.data)/8)
+        for index, label in enumerate(self.ax.xaxis.get_ticklabels()):
+            if int(index) % n != 0: label.set_visible(False)
+
+    def plotData(self):         
+        self.ax.plot(self.time, self.data)
+        updatedLegends = self.addRollingAverages()
+        plt.title(
+            'Displaying '+self.displayLabel+ ' at '+self.sourceOwner+
+            '.\n Data for '+ self.now +' from '+self.sourceID+
+            '. Max: '+self.dataMax+', Min: '+self.dataMin
+        )
         plt.xlabel('Time [hh:mm]')
-        plt.ylabel(unit_label)
-        plt.legend(legends)
+        plt.ylabel(self.unitLabel)
+        self.spaceLabels()
+        plt.legend(updatedLegends)
         plt.grid()
         plt.draw()
         plt.pause(1)
         input("<ENTER to close plot>")
-        plt.close(fig)
+        plt.close(self.fig)
 
 def main():
     Weather()
