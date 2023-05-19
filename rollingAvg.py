f0d8dcd42e5b184b3ce033bd0d0e4b6b0d8db57a
Magnus Nilsen <magnusnilsenrandaberg@hotmail.com>
2023-05-19 20:28:38 +0200

    small refactor

--- rollingAvg.py
+++ rollingAvg.py
@@ -2,28 +2,28 @@
 Calculate the rolling average for every
 element in a dataset with a windowsize
 """
-class RollingWindow:
-    def __init__(self, n):
-        self.n = n
-        self.queue = []
-    def insert(self, element):
-        self.queue.append(element)
-        if len(self.queue) > self.n:
-            self.queue.pop(0)
-    def average(self):
-        sum = 0.0
-        for element in self.queue: sum+=element
-        return sum/len(self.queue)
-
 class RollingAverage:
+    class Queue:
+        def __init__(self, n):
+            self.n = n
+            self.queue = []
+        def insert(self, element):
+            self.queue.append(element)
+            if len(self.queue) > self.n:
+                self.queue.pop(0)
+        def average(self):
+            sum = 0.0
+            for element in self.queue: sum+=element
+            return sum/len(self.queue)
+        
     def __init__(self, dataset, windowSize):
         if windowSize > len(dataset):
             print("windowsize should be smaller than dataset length.")
             return []
         self.rollingAverage = []
-        window = RollingWindow(windowSize)
+        window = self.Queue(windowSize)
         for element in dataset:
             window.insert(element)
             self.rollingAverage.append(window.average())
-    def getData(self): 
+    def get_data(self): 
         return self.rollingAverage
--- weather.py
+++ weather.py
@@ -3,122 +3,80 @@ import numpy as np
 import pandas as pd
 from datetime import date
 import matplotlib.pyplot as plt
-from dateutil import parser
 import rollingAvg
 
 class Weather:
-    """
-    Two https GET requests are chained together, so that
-    a source id for a manually inputted location is
-    requested and used to request data using the source id
-    attached to the desired location. Four observation types
-    can then be selected.
-    Rolling average can be calculated, with windowsize = 5 as default.
-    +------+
-    | FLOW |
-    +------+
-    INPUT: location:
-        -> GET(location)        -> ID
-    INPUT: element:
-        -> GET(..,ID,element)   -> DATA
-    INPUT: windowSize(s)
-        -> calculate RollingAverage(s)
-
-    plot(DATA)
-    plot(RollingAverage(s))
-    """
-    def __init__(self, 
-        source  = '',
-        element = '',
-        date    = ''
-    ):
+    def __init__(self):
         self.fig, self.ax = plt.subplots()
-        with open("secret.txt", "r") as file: self.clientID = file.readline()
+        pd.set_option('mode.chained_assignment',None)
+        with open("secret.txt", "r") as file: client_id = file.readline()
         try:
-            if not source: source = self.getSourceInput()
-            if not element: element = self.getElementInput()
-            sourceEndpoint      = 'https://frost.met.no/sources/v0.jsonld?'
-            observationEndpoint = 'https://frost.met.no/observations/v0.jsonld'
-            pd.set_option('mode.chained_assignment',None)
-            sensorsystem = self.requestData(sourceEndpoint,{'name':source})
-            sourceOwner   = sensorsystem[0]['name']
-            self.sourceOwner  = str(sourceOwner[0]+sourceOwner[1:len(sourceOwner)].lower())
-            self.sourceID = sensorsystem[0]['id']
-            observationParameters = {
-                'sources'      : self.sourceID,
-                'elements'     : element,
-                'referencetime': self.formatDate(date)
-            }
-            observations = self.requestData(observationEndpoint,observationParameters)
-            self.formatData(observations)
-            self.reportInfo()
-            self.plotData()
+            self.get_data(client_id)
+            self.plot_data()
         except Exception as e:
             print("Error: \n\t%s" %(e))
             main()
         except KeyboardInterrupt:
             exit()
-    # Prompt user for element selection
-    def getElementInput(self):
-        elementNames=['sea_water_speed','sea_surface_wave_significant_height','wind_speed','air_temperature']
-        i = int(input('Plot options:\nCurrent[1], Wave Hs[2], Wind[3], Temp[4]: '))
-        return str(elementNames[i-1])
-    # Prompt user for location name
-    def getSourceInput(self):
+
+    def get_data(self, client_id):
+        sensorsystem = self.request_data('https://frost.met.no/sources/v0.jsonld?',{'name': self.get_source_input() }, client_id)
+        source_owner   = sensorsystem[0]['name']
+        self.source_owner  = str(source_owner[0]+source_owner[1:len(source_owner)].lower())
+        self.source_id = sensorsystem[0]['id']
+        
+        observationParameters = {
+            'sources'      : self.source_id,
+            'elements'     : self.get_element_input(),
+            'referencetime': self.get_today()
+        }
+        print("Found source %s (%s)" % (self.source_id,self.source_owner))
+        self.format_data(self.request_data('https://frost.met.no/observations/v0.jsonld', observationParameters, client_id))
+
+    def get_element_input(self):
+        type = ['sea_water_speed','sea_surface_wave_significant_height','wind_speed','air_temperature']
+        opts = 'Plot options:\nCurrent[1], Wave Hs[2], Wind[3], Temp[4]: '
+        return str(type[int(input(opts))-1])
+
+    def get_source_input(self):
         supportedSources="""
             Yme
             Statfjord a/b/c
             Troll a/b/c
             Draugen
             Sola\n"""
-        print("List of some updated sources:\n%s"%supportedSources)
-        sourceName = str(input("Type location: "))
-        if sourceName in {"exit","stop","out","break","abort"}: exit()
-        return sourceName
-    # Print source ID, owner name and last entry timestamp.
-    def reportInfo(self):
-        print("Found source %s (%s)" % (self.sourceID,self.sourceOwner))
-        print("Latest entry: \t%s (%i entries)" %(str(self.time.iloc[-1]),self.time.size))
-    """
-    Param  : dateInput (format must be 'YYYY-MM-DD')
-    Return : today/tomorrow in format 'YYYY-MM-DD/YYYY-MM-DD'
-    """
-    def formatDate(self, dateInput):
-        if not dateInput: dateInput = date.today()
-        else: dateInput = parser.parse(dateInput)
-        now   = dateInput.strftime("%Y-%m-%d")
-        tomorrow = dateInput.strftime("%Y-%m-")+str(int(dateInput.strftime("%d"))+1)
+        print("List of some updated sources:\n%s" % supportedSources)
+        return str(input("Type location: "))
+
+    def get_today(self):
+        today = date.today()
+        now = today.strftime("%Y-%m-%d")
+        tomorrow = today.strftime("%Y-%m-")+str(int(today.strftime("%d"))+1)
         self.now = now
         return now+'/'+tomorrow
-    # Send a GET-request to the REST-API endpoint, return data  if successful
-    def requestData(self, endpoint, parameters):
-        r = requests.get(endpoint, parameters, auth=(self.clientID,''))
+
+    def request_data(self, endpoint, parameters, clientId):
+        r = requests.get(url = endpoint, params = parameters, auth=(clientId,' '))
         if r.status_code == 200: return np.asarray(r.json()['data'])
         else: print("Error %i, %s" %(r.status_code,r.reason))   
-    """
-    Create a dataset and corresponding legends with calculated
-    rolling average of the raw data if any windowsize is given in c-tor.
-    [module @rolling.py]
-    """
-    def addRollingAverages(self):
+
+    def add_rolling_averages(self):
         legends = ["Raw data"]
         windowSize = (int(len(self.data)/10))
-        legends.append(r"$w_{Size}$=%i" % windowSize)
-        averageData = rollingAvg.RollingAverage(self.data,windowSize).getData()   
-        self.ax.plot(self.time, averageData)
-    
+        legends.append(r"$w_{Size}$=%i" % windowSize)   
+        self.ax.plot(self.time, rollingAvg.RollingAverage(self.data,windowSize).get_data())   
         return legends
-    # Extract data recieved from the REST-API to use for plotting
-    def formatData(self, data):
+
+    def format_data(self, data):
         df = pd.DataFrame()
         for i in range(len(data)):
             row = pd.DataFrame(data[i]['observations'])
-            row['referenceTime'] = data[i]['referenceTime'][11:-8] #remove unecessary parts of timestamp
+            row['referenceTime'] = data[i]['referenceTime'][11:-8]
             row['sourceId']      = data[i]['sourceId']
             df = pd.concat([df,row])
         df = df.reset_index()
         self.unitLabel = '['+df['unit'][1]+']'
-        # Convert m/s to kts (Is only done for wind and current)
+
         if df['unit'][1]=='m/s':
             self.unitLabel = '[kts]'
             for j in range(len(df['value'])):
@@ -128,24 +86,25 @@ class Weather:
         self.displayLabel = df['elementId'][1].replace('_',' ')
         self.time = df.loc[:,'referenceTime']
         self.data = df['value']
-    # Space plot labels n entries apart
-    def spaceLabels(self):
+        print("Latest entry: \t%s (%i entries)" %(str(self.time.iloc[-1]),self.time.size))
+
+    def space_labels(self):
         n = int(len(self.data)/8)
         for index, label in enumerate(self.ax.xaxis.get_ticklabels()):
             if int(index) % n != 0: label.set_visible(False)
-    # Plot data, and calculate+plot rolling averages, if any were requested.
-    def plotData(self):         
+
+    def plot_data(self):         
         self.ax.plot(self.time, self.data)
-        updatedLegends = self.addRollingAverages()
-        plt.suptitle('Displaying '+self.displayLabel+ ' at '+self.sourceOwner)
+        updatedLegends = self.add_rolling_averages()
+        plt.suptitle('Displaying '+self.displayLabel+ ' at '+self.source_owner)
         plt.title(
-            'Data for '+ self.now +' from '+self.sourceID+
+            'Data for '+ self.now +' from '+self.source_id+
             r'. $D_{max}: $'+self.dataMax+r', $D_{min}: $ '+self.dataMin
         )
         plt.xlabel('Time')
         plt.ylabel(self.unitLabel)
         plt.ylim((float(self.dataMin)/2,float(self.dataMax)*1.1))
-        self.spaceLabels()
+        self.space_labels()
         plt.legend(updatedLegends)
         plt.grid(axis='y')
         plt.draw()
